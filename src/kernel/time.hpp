aca4ea3010f5c6a30cec1f723ba3b8d5b72be9bf
Ayush <ayushthapa026@gmail.com>
2026-04-30 16:51:29 -0700

    feat: added rdtsc

---
+++ src/kernel/time.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+
+namespace queen {
+    inline uint64_t read_tsc() {
+        uint64_t value = 0;
+        asm volatile("rdtsc");
+
+        uint32_t eax_value, edx_value;
+        asm volatile("movl %%eax, %0" : "=r"(eax_value));
+        asm volatile("movl %%edx, %0" : "=r"(edx_value));
+
+        value |= eax_value;
+        value |= (edx_value << 31);
+
+        return value;
+    }
+}
