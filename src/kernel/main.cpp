b6d4774fa5f8e58c151383aaf55b0dcc9f604f26
Ayush <ayushthapa026@gmail.com>
2026-04-30 16:51:54 -0700

    test: added rdtsc testing logic in kernel_main

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,8 +1,15 @@
 #include "serial.hpp"
+#include "time.hpp"
+#include <stdint.h>
 
 extern "C" void kernel_main() {
     queen::serial::init();
+    uint64_t start = queen::read_tsc();
     queen::serial::write("queen booted\n");
+    uint64_t end = queen::read_tsc();
+
+    uint64_t delta = end - start;
+    queen::serial::write_hex(delta);
 
     for (;;) {
         asm volatile("hlt");
