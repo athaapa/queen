ee41a91c016e270a19b69a23bf9f5eb50438d7e2
Ayush <ayushthapa026@gmail.com>
2026-04-29 20:02:36 -0700

    build: cleaned up build workflow

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,19 +1,6 @@
 #include "serial.hpp"
-#include <cstdint>
-
-static inline void debugcon_write_char(char c) {
-    asm volatile("outb %0, %1" : : "a"(static_cast<uint8_t>(c)), "Nd"(0xE9));
-}
-
-static void debugcon_write(const char* s) {
-    while (*s != '\0') {
-        debugcon_write_char(*s++);
-    }
-}
 
 extern "C" void kernel_main() {
-    debugcon_write("entered kernel_main\n");
-
     queen::serial::init();
     queen::serial::write("queen booted\n");
 
